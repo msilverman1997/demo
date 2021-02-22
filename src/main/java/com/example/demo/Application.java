d8fd5e1447660a13a8b9666ca1ec0c95e0fed12b
Michael <msilverman1997@gmail.com>
2021-02-22 12:33:25 -0500

    Loaded Database

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(PathRepository repository){
+		return (args) -> {
+			repository.save(new Path("NYC", "Chicago", "1000", "Large", "Monday"));
+			repository.save(new Path("LA", "D.C.", "2500", "Large", "Monday"));
+			repository.save(new Path("Dallas", "Kansas City", "300", "Large", "Monday"));
+			repository.save(new Path("Atlanta", "Miami", "350", "Large", "Monday"));
+			repository.save(new Path("Boston", "NYC", "200", "Large", "Monday"));
+
+			log.info("Database Values Initialized");
+
+		};
 	}
 
 }
